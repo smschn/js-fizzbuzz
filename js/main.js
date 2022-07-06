35853ca15f095fae46e4734930c18bd3e1662a1a
Luca Somaschini <somas99@gmail.com>
2022-07-06 15:15:17 +0200

    Completed js - console.log only

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-for (i=0; i<=100; i++) {
+let i;
+
+for (i=1; i<=100; i++) {
     console.log(i);
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
+    } else if (i % 5 == 0) {
+        console.log('Buzz');
+    } else if (i % 3 == 0) {
+        console.log('Fizz');
+    }
 }
