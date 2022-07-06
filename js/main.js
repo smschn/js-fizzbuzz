09f81cb77adac40c7c725067217031bc5898f4a9
Luca Somaschini <somas99@gmail.com>
2022-07-06 15:36:18 +0200

    Js - generate boxes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 let i;
 
+const containerDom = document.getElementById('container');
+
+// for loop
 for (i=1; i<=100; i++) {
+
+    // generate boxes for each iteration
+    const newBox = document.createElement('div');
+    newBox.append(i);
+    newBox.className = "box";
+    containerDom.append(newBox);
+
     if (i % 3 == 0 && i % 5 == 0) {
         console.log('FizzBuzz');
     } else if (i % 5 == 0) {
